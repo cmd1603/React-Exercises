75a52576b4e81a6b646f2ed3e16a64f7d8692915
Chris Davila <cmd1603@gmail.com>
2018-04-22 10:45:56 -0500

    Creating and adding components exercise

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import MyComponent from './MyComponent';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.onSubmit = this.onSubmit.bind(this);
+    this.state = {
+      title: 'App title'
+    }
+
+    this.onClick = this.onClick.bind(this);
   }
 
-  onSubmit(event) {
-    event.preventDefault();
-    console.log(this.input.value);
+  onClick() {
+    this.setState({
+      title: 'New app title'
+    });
   }
+
   render() {
-    const list = [
-      'Item 1',
-      'Item 2',
-      'Another Item'
-    ];
+
     return (
       <div className="App">
-        <h1>{
-            list.map(item => {
-              return (
-                  <div key={item} onMouseEnter={this.onMouseEnter}>{item}</div>
-                );
-            })
-        }
-        </h1>
-        <form onSubmit={this.onSubmit}>
-          <input onChange={this.onChange} ref={input => this.input = input} />
-        </form>
+        <h1>{this.state.title}</h1>
+        <div onClick={this.onClick}>Click Here!</div>
+        <MyComponent />
       </div>
     );
   }
